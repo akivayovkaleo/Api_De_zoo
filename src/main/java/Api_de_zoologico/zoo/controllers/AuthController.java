2e3aeff25f016afe36c06305b80a53a7dc550115
Vinícius Stropoli <vini.stropoli@gmail.com>
2025-09-22 09:02:58 -0300

    Security JWT implementado

--- src/main/java/Api_de_zoologico/zoo/controllers/AuthController.java
+++ src/main/java/Api_de_zoologico/zoo/controllers/AuthController.java
@@ -1,4 +1,40 @@
 package Api_de_zoologico.zoo.controllers;
 
+import Api_de_zoologico.zoo.security.JwtUtil;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        String token = jwtUtil.generateToken(request.getUsername());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @Data
+    static class AuthRequest {
+        private String username;
+        private String password;
+    }
+
+    @Data
+    static class AuthResponse {
+        private final String token;
+    }
 }
